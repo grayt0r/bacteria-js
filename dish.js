81cb8a700866a140e3a40e38f805dea3c230a1d4
Ross Grayton <rossgrayton@gmail.com>
2014-03-05 10:27:48 +0000

    Make methods more functional

--- dish.js
+++ dish.js
@@ -1,46 +1,53 @@
 var Dish = function(input) {
-  this.setLiveCells(input);
-  this.calculateNextGeneration();
+  this.cells = this.buildLiveCells(input);
+  this.step();
 };
 
 function parseKey(str) {
-  return str.split('-').map(function(l) {
+  return str.split(',').map(function(l) {
     return parseInt(l, 10);
   });
 }
 
 function makeKey(arr) {
-  return arr[0] + '-' + arr[1];
+  return arr[0] + ',' + arr[1];
 }
 
 function neighbours(x, y) {
   return [[x-1, y-1], [x, y-1], [x+1, y-1], [x-1, y], [x+1, y], [x-1, y+1], [x, y+1], [x+1, y+1]];
 }
 
-Dish.prototype.setLiveCells = function(input) {
-  this.cells = {};
+Dish.prototype.step = function() {
+  this.cells = this.buildLiveCells(this.calculateNextGeneration());
+  this.print();
+}
+
+Dish.prototype.cellList = function() {
+  return Object.keys(this.cells);
+}
+
+Dish.prototype.buildLiveCells = function(input) {
+  var cells = {};
   input.forEach(function(l) {
-    this.cells[makeKey(l)] = true;
+    cells[makeKey(l)] = true;
   }, this);
+  return cells;
 };
 
 Dish.prototype.cellsWithSurrounding = function() {
   var res = {};
-  Object.keys(this.cells).forEach(function(c) {
+  this.cellList().forEach(function(c) {
     res[c] = true;
     var l = parseKey(c);
     neighbours(l[0], l[1]).forEach(function(n) {
       res[makeKey(n)] = true
     }, this);
   }, this);
-  
   return Object.keys(res);
 };
 
 Dish.prototype.calculateNextGeneration = function() {
   var nextGen = [];
-  
-  console.log('Next generation:');
   
   this.cellsWithSurrounding().forEach(function(c) {
     var l = parseKey(c);
@@ -49,12 +56,11 @@ Dish.prototype.calculateNextGeneration = function() {
     
     // Rule 2 || Rule 4
     if((isAlive && (count === 2 || count === 3)) || (!isAlive && count === 3)) {
-      console.log(l.join(','));
       nextGen.push(l);
     }
   }, this);
   
-  this.setLiveCells(nextGen);
+  return nextGen;
 };
 
 Dish.prototype.calculateNumberOfLiveNeighbours = function(l) {
@@ -62,6 +68,13 @@ Dish.prototype.calculateNumberOfLiveNeighbours = function(l) {
   return neighbours(l[0], l[1]).reduce(function(total, n) {
     return (that.cells[makeKey(n)]) ? total + 1 : total;
   }, 0);
+};
+
+Dish.prototype.print = function() {
+  console.log('Active cells:');
+  this.cellList().forEach(function(c) {
+    console.log(c);
+  });
 };
 
 // TODO: Cell class with x, y?
