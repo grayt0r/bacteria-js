75a7959150868f7ca54aa3e01f75038c320b1736
Ross Grayton <rossgrayton@gmail.com>
2014-03-05 13:06:36 +0000

    Tweak eachCell helper

--- dish.js
+++ dish.js
@@ -1,34 +1,31 @@
 var Cell = require('./cell');
 
 var Dish = function(input) {
-  this.cells = this.buildLiveCells(input);
+  // input is an array of location arrays (e.g. [[1, 2], [2, 2], [3, 2]])
+  this.cells = this.buildCells(input);
 };
 
-Dish.prototype.simulate = function(generations) {
-  for(var i = 0; i < generations; i++) {
-    this.cells = this.buildLiveCells(this.calculateNextGeneration());
-    this.print(i + 1);
-  }
-}
-
-Dish.prototype.eachCell = function(cells, f) {
-  if(f === undefined) {
-    f = cells;
-    cells = this.cells;
-  }
-  
-  Object.keys(cells).forEach(function(k) {
-    f.call(this, k, cells[k]);
-  }, this);
-}
-
-Dish.prototype.buildLiveCells = function(input) {
+Dish.prototype.buildCells = function(input) {
   var cells = {};
   input.forEach(function(l) {
     var cell = new Cell(l[0], l[1]);
     cells[cell.key()] = cell;
   }, this);
   return cells;
+};
+
+Dish.prototype.simulate = function(generations) {
+  for(var i = 0; i < generations; i++) {
+    this.cells = this.buildCells(this.calculateNextGeneration());
+    this.print(i + 1);
+  }
+};
+
+Dish.prototype.eachCell = function(f, cells) {
+  cells = cells || this.cells;
+  Object.keys(cells).forEach(function(k) {
+    f.call(this, k, cells[k]);
+  }, this);
 };
 
 Dish.prototype.cellsWithSurrounding = function() {
@@ -45,15 +42,15 @@ Dish.prototype.cellsWithSurrounding = function() {
 Dish.prototype.calculateNextGeneration = function() {
   var nextGen = [];
   
-  this.eachCell(this.cellsWithSurrounding(), function(k, c) {
+  this.eachCell(function(k, c) {
     var count = this.calculateNumberOfLiveNeighbours(c);
-    var isAlive = !!this.cells[c.key()]
+    var isAlive = !!this.cells[c.key()];
     
     // Rule 2 || Rule 4
     if((isAlive && (count === 2 || count === 3)) || (!isAlive && count === 3)) {
       nextGen.push([c.x, c.y]);
     }
-  });
+  }, this.cellsWithSurrounding());
   
   return nextGen;
 };
